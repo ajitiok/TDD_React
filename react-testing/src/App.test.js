a48d1cbb2776915b699a40979117bc8af08502e0
ajitiok <aji@sonar.id>
2022-02-24 18:00:40 +0700

    testing react 17

---
+++ react-testing/src/App.test.js
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+import { configure,shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+configure({ adapter: new Adapter() });
+
+describe('Counter Testing', () => {
+
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+
+  })
+
+  test('renders the title of counter', () => {
+    expect(wrapper.find('h1').text()).toContain('This is counter app');
+  });
+
+  test('render a button with text of `increment`', () => {
+    expect(wrapper.find('#increment-btn').text()).toBe('Increment')
+  })
+
+  test('render the initial value of state in a div', () => {
+    expect(wrapper.find('#counter-value').text()).toBe("0");
+  })
+
+  test('render the click event of increment button and increment counter value', () =>{
+    wrapper.find('#increment-btn').simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('1');
+  } )
+
+  test('render the click event of decrement button and decrement counter value', () =>{
+    wrapper.find('#increment-btn').simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('1');
+    wrapper.find('#decrement-btn').simulate('click')
+    expect(wrapper.find('#counter-value').text()).toBe('0');   
+  } )
+});
+
+  
